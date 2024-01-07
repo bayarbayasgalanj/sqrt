bfee15838a7cb11c79d67d3cdbeb5ed10d70f56a
Bayasaa <bayarbayasgalan.j@gmail.com>
2024-01-06 18:52:31 -0600

    day1

---
+++ sol2.cpp
@@ -0,0 +1,11 @@
+#include <iostream> // Энд юу бичдэг билээ?
+using namespace std;
+
+int main() {
+    int a;
+    cout << "sdsdfsd-------------";
+    cin >> a;
+    cout << "result-------------"<<a;
+    // Энд юу бичих вэ?
+    return 0;
+}
---
+++ sol3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int lucky;
+    lucky = 9;
+    if (0 < lucky && lucky < 10){
+        cout << lucky << endl;
+    }
+        
+    return 0;
+}
